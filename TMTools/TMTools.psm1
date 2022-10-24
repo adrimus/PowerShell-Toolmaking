7a98e307fc3e50af710badf3dd3e797c70be0696
adrianm <adrian.muscat@systech.net>
2022-10-24 16:05:01 +0200

    convert to advanced function and added validation

--- TMTools/TMTools.psm1
+++ TMTools/TMTools.psm1
@@ -1,28 +1,35 @@
 function Get-TMRemoteListeningConfiguration {
     [CmdletBinding()]
     param (
-        # Parameter help description
-        [Parameter(ValueFromPipeline)]
-        [string[]]
-        $ComputerName="localhost",
 
-        # Parameter help description
-        [Parameter()]
-        [string]
-        $ErrorLog
+        [Parameter(ValueFromPipeline = $true, Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [Alias("CN")]
+        [string[]]$ComputerName,
+
+        [string]$ErrorLog
     )
-        
+    Begin {
+        #not used
+    }
+
     process {
-        $ports = 22,5985,5986
+
+        $ports = 22, 5985, 5986
         foreach ($computer in $ComputerName) {
             foreach ($port in $ports) {
                 Test-NetConnection -port $port -ComputerName $computer |
-                Select-Object ComputerName,RemotePort,TCPTestSucceeded
+                Select-Object ComputerName, RemotePort, TCPTestSucceeded
             } #foreach
         } #foreach
         #TODO
         #better output
         #error handling and logging
+
     } #process
+
+    End {
+        #not used
+    }
 
 } #function Get-TMRemoteListeningConfiguration
